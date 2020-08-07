bb1ca2ed906c7592765811d3347e5d812bebb0ad
Davi Bernardo <dnssites@gmail.com>
2020-08-07 19:12:45 -0300

    Fatiando o Layout e Controlando Rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WebController@home')->name('home');
+Route::get('/curso', 'WebController@course')->name('course');
+Route::get('/blog', 'WebController@blog')->name('blog');
+Route::get('/blog/{uri}', 'WebController@article')->name('article');
+Route::get('/contato', 'WebController@contact')->name('contact');
